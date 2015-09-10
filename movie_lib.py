9958c4674232664e5e6f6fe6ddad3e8c63326031
William Butts <wbutts1023@gmail.com>
2015-09-10 17:00:48 -0400

    Created a baselline class for each of movie, user, and rating.

--- movie_lib.py
+++ movie_lib.py
@@ -1,1 +1,17 @@
-This is where the code lives!
+class Movie():
+
+    def __init__(movie_id,movie_title):
+        self.movie_id = movie_id
+        self.movie_title = movie_title
+
+
+
+class Rating():
+
+    def __init__(rating):
+        self.rating = rating
+
+class User():
+
+    def __init__(user_id):
+        self.user_id = user_id

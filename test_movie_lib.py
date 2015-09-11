3dc6e1f990d84292dbe90a7f7a4d97cfe21511e8
William Butts <wbutts1023@gmail.com>
2015-09-11 00:51:55 -0400

    Figured out how to run tests on methods using instances.  Passed all tests on class methods

--- test_movie_lib.py
+++ test_movie_lib.py
@@ -1,14 +1,15 @@
 from movie_lib import *
-
+will = User('Will')
+movie = Movie(42)
 
 def test_get_rating():
-    assert get_rating(42) == [3]
+    assert movie.get_rating() == [3]
 
 def test_get_average_rating():
-    assert get_average_rating(42) == 3
+    assert movie.get_average_rating() == 3
 
 def test_get_movie_title():
-    assert get_movie_title(42) == 'Star Wars'
+    assert movie.get_movie_title() == 'Star Wars'
 
 def test_get_user_ratings():
-    assert get_user_ratings('Will') == [[42,3]]
+    assert will.get_user_ratings() == [[42,3]]

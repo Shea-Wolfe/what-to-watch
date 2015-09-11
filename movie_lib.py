8f19769572973df93545feb9b08af64966271ed7
William Butts <wbutts1023@gmail.com>
2015-09-11 13:10:29 -0400

    Finished refactoring classes, currently writing tests for class creation.  User creation test passing

--- movie_lib.py
+++ movie_lib.py
@@ -1,15 +1,15 @@
-all_movies = {}}
+all_movies = {}
 all_users = {}
 class Movie:
 
     def __init__(self, movie_id, title):
-        self.movie_id = movie_id
+        self.id = movie_id
         self.title = title
         all_movies[self.id] = self
         self.ratings = {} #key: user_id, value: Rating objects
 
     def add_rating(self, rating): #rating is an instance of CLASS rating.
-        return self.ratings[rating.user_id] = rating.score
+        self.ratings[rating.user] = rating.score
 
     def __str__(self):
         return 'Movie(movie_id={}, title={})'.format(self.id, __repr__(self.title))
@@ -26,32 +26,38 @@ class Movie:
     def get_movie_title(self):
         '''Takes a movie_id and returns the movie title.'''
         return self.title
-class Rating:
-
-    def __init__(self,user, movie, score):
-        self.score = score
-        self.user = user
-        self.movie = movie
-        all_movies[self.movie].add_rating(self)
-
-    def __str__(self):
-        return 'Rating(user={}, movie={}, score={})'.format(self.user, self.movie, self.score))
-
-    def __repr__(self):
-        return self.__str__
+# class Rating:
+#
+#     def __init__(self,user, movie, score):
+#         self.score = score
+#         self.user = user
+#         self.movie = movie
+#         all_movies[self.movie].add_rating(self)
+#         all_users[self.user].add_user_rating(self)
+#
+#     def __str__(self):
+#         return 'Rating(user={}, movie={}, score={})'.format(self.user, self.movie, self.score))
+#
+#     def __repr__(self):
+#         return self.__str__
 
 
 class User:
 
     def __init__(self, user_id):
-        self.user_id = user_id
+        self.id = user_id
         all_users[self.id] = self
+        self.ratings = {} #Key Movie_id, value rating score
 
-    def get_user_ratings(self):
-        '''takes a user_id and returns all the ratings done by that user'''
-        user_ratings = []
-        ratings_list = [['Will', 42, 3],['Steve', 47, 5]]
-        for rating in ratings_list:
-            if rating[0] == self.user_id:
-                user_ratings.append([rating[1],rating[2]])
-        return user_ratings
+    def add_user_rating(self, rating): #Rating is an instance of the CLASS rating
+        self.ratings[rating.movie] = rating.score
+
+
+    # def get_user_ratings(self):
+    #     '''takes a user_id and returns all the ratings done by that user'''
+    #     user_ratings = []
+    #     ratings_list = [['Will', 42, 3],['Steve', 47, 5]]
+    #     for rating in ratings_list:
+    #         if rating[0] == self.user_id:
+    #             user_ratings.append([rating[1],rating[2]])
+    #     return user_ratings
--- test_movie_lib.py
+++ test_movie_lib.py
@@ -1,15 +1,27 @@
 from movie_lib import *
-will = User('Will')
-movie = Movie(42)
+will = User(1)
+steve = User(2)
+movie1 = Movie(15, 'The Matrix')
+movie2 = Movie(20, 'Brick')
 
-def test_get_rating():
-    assert movie.get_rating() == [3]
 
-def test_get_average_rating():
-    assert movie.get_average_rating() == 3
+def test_user_creation():
+    assert will.id == 1
+    assert steve.id == 2
+    assert will.id != steve.id
 
-def test_get_movie_title():
-    assert movie.get_movie_title() == 'Star Wars'
 
-def test_get_user_ratings():
-    assert will.get_user_ratings() == [[42,3]]
+
+
+# def test_get_rating():
+#     #Should return a list of Rating objects
+#     assert movie.get_rating() == [3]
+#
+# def test_get_average_rating():
+#     assert movie.get_average_rating() == 3
+#
+# def test_get_movie_title():
+#     assert movie.get_movie_title() == 'Star Wars'
+#
+# def test_get_user_ratings():
+#     assert will.get_user_ratings() == [[42,3]]

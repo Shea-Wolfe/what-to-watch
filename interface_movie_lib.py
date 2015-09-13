dc5cdd870588a13b7e57880a26173c60699a7890
William Butts <wbutts1023@gmail.com>
2015-09-13 01:57:22 -0400

    Added a way to view all reviews the current user has done and to add new (non persistant) reviews to the database

--- interface_movie_lib.py
+++ interface_movie_lib.py
@@ -16,6 +16,12 @@ def main():
         elif menu_choice == 3:
             print(find_similar_user(current_user))
         elif menu_choice == 4:
+            list_all_movies()
+        elif menu_choice == 5:
+            add_review(current_user)
+        elif menu_choice == 6:
+            print(all_users[current_user].ratings)
+        elif menu_choice == 7:
             break
 
 
@@ -39,16 +45,19 @@ def get_user_id():
 
 def get_menu_choice():
     menu_choice = input('''\nPlease select your option from those below
-    [1] get a listing of top movies
-    [2] get a movie recomendation
-    [3] get a recomendation from a similar user
-    [4] exit the program\n > ''')
+    [1] Get a listing of top movies.
+    [2] Get a movie recomendation.
+    [3] Get a recomendation from a similar user.
+    [4] View all the movies in the database.
+    [5] Add a new review.
+    [6] View your existing reviews.
+    [7] Exit the program.\n > ''')
     try:
         menu_choice = int(menu_choice)
     except:
         print('That is not number. Try again!')
         return get_menu_choice()
-    if menu_choice > 4 or menu_choice < 1:
+    if menu_choice > 7 or menu_choice < 1:
         print('That is not one of the options.  Try again!')
         return get_menu_choice()
     return menu_choice
@@ -68,6 +77,39 @@ def number_of_movies(all_movies=all_movies):
     movie_dict = get_top_x(movie_num)
     for item in movie_dict:
         print(all_movies[item[0]].title)
+
+def list_all_movies(all_movies=all_movies):
+    for movie in all_movies:
+        print(movie, all_movies[movie].title)
+
+def add_review(current_user):
+    movie = input('''Please enter the ID of the movie you want to review.
+    enter [movies] for a list of all movie IDs with titles.
+    > ''')
+    if movie == 'movies' or movie == 'm':
+        list_all_movies()
+    else:
+        try:
+            movie = int(movie)
+        except:
+            add_review(current_user)
+        if movie < 0 or movie > 1682:
+            print('Sorry, I do not have that movie in my database.\n Try again.')
+            return add_review(current_user)
+        else:
+            print('Great, you have decided to review {}'.format(all_movies[movie].title))
+            while True:
+                try:
+                    score = int(input('Please enter your review score from 1 to 5 with 1 being the lowest.\nWhole numbers only please.\n> '))
+                except:
+                    print('That is not a valid score. Try again.')
+                    continue
+                if score > 0 and score < 6:
+                    print('Review added.  You gave {} a {} out of 5'.format(all_movies[movie].title,score))
+                    Rating(current_user, movie, score)
+                    break
+                elif score < 0 or score > 5:
+                    print('Please enter a score from 1 to 5.')
 
 
 if __name__ == '__main__':
